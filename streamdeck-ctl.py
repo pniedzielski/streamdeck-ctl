7bcd07cf2e0e455f0f95e61bf21c994802ebe57b
Patrick M. Niedzielski <patrick@pniedzielski.net>
2024-08-01 00:37:35 +0000

    Add CPU% indicator routine

--- streamdeck-ctl.py
+++ streamdeck-ctl.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python3
 
+from PIL import Image, ImageDraw, ImageFont
+import psutil
 from StreamDeck.DeviceManager import DeviceManager
+from StreamDeck.ImageHelpers import PILHelper
 import sys
+
+def render_labeled_number(deck, number, label):
+    center_font = ImageFont.load_default(size=48)
+    label_font = ImageFont.load_default(size=14)
+
+    key_image = PILHelper.create_key_image(deck)
+    draw = ImageDraw.Draw(key_image)
+    draw.text(
+        (key_image.width / 2, key_image.height / 2),
+        text=str(number),
+        font=center_font,
+        anchor="ms",
+        fill="white"
+    )
+    draw.text(
+        (key_image.width / 2, 7 * key_image.height / 8),
+        text=label,
+        font=label_font,
+        anchor="ms",
+        fill="white"
+    )
+
+    return PILHelper.to_native_key_format(deck, key_image)
+
+def update_cpu_percent(deck, key_number):
+    key_image = render_labeled_number(deck, psutil.cpu_percent(), "CPU %")
+    with deck:
+        deck.set_key_image(key_number, key_image)
 
 def main():
     stream_decks = DeviceManager().enumerate()
@@ -24,6 +55,8 @@ def main():
     deck.open()
     deck.reset()
 
+    deck.set_brightness(50)
+
     deck_type = deck.deck_type()
     if deck_type == "":
         deck_type = "Stream Deck"
@@ -34,6 +67,8 @@ def main():
     Product ID: {deck.product_id()}
     Serial Number: {deck.get_serial_number()}
     Firmware Version: {deck.get_firmware_version()}""")
+
+    update_cpu_percent(deck, 5)
 
     # Wait for user to kill the process
     while True:
